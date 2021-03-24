1fd786bc8a7eb72e81fb2f036d0ce1aeb5ca77fc
Orlando Álvarez <wiki97ap@gmail.com>
2021-03-24 14:48:25 -0600

    Flatlist fix
    Fixed flatlist error
    
    Co-Authored-By: Eduardo Salazar <63610573+Seal-Trainer@users.noreply.github.com>

--- recetario/components/HorizontalList.js
+++ recetario/components/HorizontalList.js
@@ -1,74 +1,45 @@
 import React from 'react'
-import { View, Text, StyleSheet, FlatList, TouchableOpacity, Image, SafeAreaView } from 'react-native'
-//import meals from './recetas'
-
-const DATAPRUEBA = [
-    {
-        id:"1",
-        name: "prueba1"        
-    },
-    {
-        id:"2",
-        name: "prueba2"        
-    },
-    {
-        id:"3",
-        name: "prueba3"        
-    },
-    {
-        id:"4",
-        name: "prueba4"        
-    },
-    {
-        id:"5",
-        name: "prueba5"       
-    }
-]
+import { Text, StyleSheet, FlatList, TouchableOpacity, Image, SafeAreaView } from 'react-native'
+import meals from './recetas'
 
 const HorizonalList = ({ title }) => {
-    
 
-    const itemList = ({item})=>{
-        
-        <TouchableOpacity style={styles.item}>            
-            <Text style={styles.textitem}>{item.name}</Text>         
+
+    const itemList = ({ item }) => (
+        <TouchableOpacity style={styles.item}>
+            <Text style={styles.textitem}>{item.name}</Text>
+            <Image style={styles.imagen} source={{ uri: item.imgUrl }} />
         </TouchableOpacity>
-    }
+    )
 
     return (
         <SafeAreaView style={styles.container}>
             <Text style={styles.title}>{title}</Text>
-            <FlatList style = {{backgroundColor: 'yellow'}}
-                data={DATAPRUEBA}
+            <FlatList style={{}}
+                data={meals}
                 renderItem={itemList}
                 keyExtractor={(item) => item.id}
                 horizontal={true}
-            />           
+            />
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {        
-        flex:1,
+    container: {
+        flex: 1,
         padding: 10,
     },
     title: {
         color: '#F50087',
         fontSize: 50
     },
-    textitem: {       
+    textitem: {
         color: 'white',
-        fontSize: 20,
-
-    },
-    image:{  
-        width:'100%',
-        height:'100%'           
-    },
-    item:{        
-        width:20,
-        height:20
+        fontSize: 25,
+    }, imagen:{
+        height:55,
+        width:55
     }
 })
 
